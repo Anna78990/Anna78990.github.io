1c718c5ee7eba3b3c303986dc0c93891f62a078f
Anna78990 <takasuanna1990@gmail.com>
2023-07-12 10:07:44 +0200

    hp

---
+++ js/vegas.js
@@ -0,0 +1,30 @@
+$(function() {
+    $('.mainimg-tate').vegas({
+        slides: [
+            { src: './images/programming.jpg' },	//1枚目の写真指定
+            { src: './images/book.jpg' },	//2枚目の写真指定
+            { src: './images/trip.jpg' },	//3枚目の写真指定
+        ],
+		transition: 'blur',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
+		animation: 'kenburns',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
+		delay: 6000,				//次の画像を表示するまでの時間
+		animationDuration: 10000,	//アニメーション間の引き継ぎタイミング。
+		timer: false,				//プログレスバーを非表示に。
+    });
+});
+
+
+$(function() {
+    $('.mainimg-yoko').vegas({
+        slides: [
+            { src: './images/programming.jpg' },	//1枚目の写真指定
+            { src: './images/book.jpg' },	//2枚目の写真指定
+            { src: './images/trip.jpg' },	//3枚目の写真指定
+        ],
+		transition: 'blur',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
+		animation: 'kenburns',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
+		delay: 6000,				//次の画像を表示するまでの時間
+		animationDuration: 10000,	//アニメーション間の引き継ぎタイミング。
+		timer: false,				//プログレスバーを非表示に。
+    });
+});
